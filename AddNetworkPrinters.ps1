ada5bb96312ac39cd6f9dd8c449fdd217ac8b4d3
Sergei Volkov <vkvsrg@gmail.com>
2023-06-28 19:08:59 +0300

    edit script AddNetworkPrinters.ps1

--- AddNetworkPrinters.ps1
+++ AddNetworkPrinters.ps1
@@ -1,14 +1,14 @@
- установить все доступные принтеры на сервере печати на УДАЛЕННОМ компьютере пользователя:
-$RemoteComputer = "ws00sr12"
-$ServerName = "sn001sradhcpprt"
-$Printers = Get-Printer -ComputerName $ServerName | Where-Object {$_.PrinterStatus -ne 'Offline' -and $_.Shared -eq 'True'}
-foreach ($Printer in $Printers){
+#Scan Print server for Shared printers and connect its on remote computer
+$RemoteComputer = "COMPUTERNAME"
+$ServerName = "PRINTSERVER"
+$Printers = Get-Printer -ComputerName $ServerName | Where-Object { $_.PrinterStatus -ne 'Offline' -and $_.Shared -eq 'True' }
+foreach ($Printer in $Printers) {
     $prt = "\\$ServerName\$($Printer.ShareName)"
     Invoke-Command -ComputerName $RemoteComputer -ScriptBlock {
         Param($prt)
         Write-Output "Adding $prt"
         iex "RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /n""$prt"""
-        } -ArgumentList $prt
+    } -ArgumentList $prt
 }
 Start-Sleep -Seconds 10
 Invoke-Command -ComputerName $RemoteComputer -ScriptBlock {
@@ -16,4 +16,4 @@ Invoke-Command -ComputerName $RemoteComputer -ScriptBlock {
     Stop-Service -Name spooler -Force
     Start-Sleep -Seconds 5
     Start-Service -Name spooler
-    }
+}
